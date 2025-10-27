6acbd01b0744b59f2342c4fa9fd143002580cd42
Maged Groups <magedyaseengroups@gmail.com>
2025-10-27 21:34:29 +0300

    Fix: Str methods to return string

--- app/traits/Str.php
+++ app/traits/Str.php
@@ -5,18 +5,23 @@ trait Str
 {
     public static function humanize(string $string): string
     {
+        return 'humanize';
     }
     public static function humanizePlural(string $string): string
     {
+        return 'humanizePlural';
     }
     public static function humanizeSingular(string $string): string
     {
+        return 'humanizeSingular';
     }
     public static function sanitize(string $string): string
     {
+        return 'sanitize';
     }
     public static function slug(string $string): string
     {
+        return 'slug';
     }
 
 }
